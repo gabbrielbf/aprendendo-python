8c5be0e1d5a45b04508204ef19948337490880bc
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-06-07 00:04:45 -0300

    adjust of my option 1 in menu. Incrementing a new question about adict a new user or not

--- projetos/agenda.py
+++ projetos/agenda.py
@@ -1,6 +1,6 @@
 def exibir_agenda():
     """ Função de exibição do menu """
-    
+
     print(' --- Minha agenda --- ')
     print('O que deseja fazer: ')
     print()
@@ -17,31 +17,53 @@ def exibir_agenda():
 def adicionar_contato(nome, telefone, email):
     """ Função para adicionar um usuário """
 
-    if minha_agenda == '':
-        minha_agenda = {}
+    for contato in minha_agenda:
 
-    """ Parei aqui tentando solucionar a lógica da adição de adição 
-    de um novo dicionário com os dados do usuário """
-
-    for contato in range(1, len(minha_agenda)):
-
-        minha_agenda[contato] = {'nome': None, 'telefone': None,
-                    'email': None, 'favorito': None}
+        """ O looping FOR está duplicando o conteúdo das variáveis dentro
+        das chave: valor na minha_agenda, pensar em uma forma de conferir se a chave está
+        vazia antes de preencher. """
 
         minha_agenda[contato]['nome'] = nome
         minha_agenda[contato]['telefone'] = telefone
         minha_agenda[contato]['email'] = email
 
 
-minha_agenda = {}
+def adicionar_mais_alguem():
+    print()
+    print('1 - Adicionar mais alguém')
+    print('0 - Voltar ao MENU')
+    print('----------')
+    adicionar_outro = int(input('Deseja adicionar mais alguém? '))
 
-variavel_temporaria = {1: {'nome': None, 'telefone': None,
+    match adicionar_outro:
+        case 1:
+            return True
+        case 0:
+            return False
+
+
+minha_agenda = {1: {'nome': None, 'telefone': None,
+                    'email': None, 'favorito': None},
+                2: {'nome': None, 'telefone': None,
+                    'email': None, 'favorito': None},
+                3: {'nome': None, 'telefone': None,
+                    'email': None, 'favorito': None},
+                4: {'nome': None, 'telefone': None,
                     'email': None, 'favorito': None}}
+
 
 match exibir_agenda():
     case 1:
-        nome = input('Digite o nome do contato: ')
-        telefone = input('Digite o telefone do contato: ')
-        email = input('Digite o e-mail do contato: ')
-        adicionar_contato(nome, telefone, email)
+        while True:
+            nome = input('Digite o nome do contato: ')
+            telefone = input('Digite o telefone do contato: ')
+            email = input('Digite o e-mail do contato: ')
+            adicionar_contato(nome, telefone, email)
+            print(minha_agenda)
 
+            if adicionar_mais_alguem() == True:
+                continue
+            else:
+                print('Voltando para o menu...')
+                print()
+                break
