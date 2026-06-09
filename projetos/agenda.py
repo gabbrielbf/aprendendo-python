1907665f8eccb232f028718afc332bc0f1fff893
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-06-08 21:06:19 -0400

    finish option 1 and 2. Now, my program will do aditc, edit, and see all users

--- projetos/agenda.py
+++ projetos/agenda.py
@@ -1,7 +1,10 @@
+import os
+import time
+
 def exibir_agenda():
     """ Função de exibição do menu """
 
-    print(' --- Minha agenda --- ')
+    print(' ----- Minha agenda ----- ')
     print('O que deseja fazer: ')
     print()
     print('1 - Salvar contato')
@@ -15,11 +18,11 @@ def exibir_agenda():
 
 
 def adicionar_contato(nome, telefone, email):
-    """ Função para adicionar um usuário """
+    """ Função para adicionar um contato """
 
     minha_agenda.append({'nome': nome, 'telefone': telefone,
                           'email': email, 'favorito': False})
-
+    print()
     print(f'Contato: {nome} | {telefone} adicionado com sucesso!')
     return
 
@@ -31,7 +34,7 @@ def adicionar_mais_alguem():
     print()
     print('1 - Adicionar mais alguém')
     print('0 - Voltar ao MENU')
-    print('----------')
+    print('-' * 20)
     while True:
         adicionar_outro = int(input('Deseja adicionar mais alguém? '))
         match adicionar_outro:
@@ -61,12 +64,57 @@ def visualizar_contatos(agenda):
     return 
 
 
-def editar_agenda(agenda, indice_contato):
-    print('Qual contato deseja editar: ')
-    visualizar_contatos(minha_agenda)
+def editar_somente_nome(agenda, indice_contato, nome):
+    """ Função designada apenas para edição de nome do usuário """
 
+    indice_contato_correto = indice_contato - 1
+    if indice_contato_correto >= 0 and indice_contato_correto < len(minha_agenda):
+        agenda[indice_contato_correto]['nome'] = nome
+        print(f'Contato {indice_contato} atualizado com sucesso!')
+    else:
+        print('[ERRO]! O Indice é inválido!')
+        print()
+    return
     
+
+def editar_somente_telefone(agenda, indice_contato, telefone):
+    """ Função designada apenas para edição de telefone do usuário """
+
+    indice_contato_correto = indice_contato - 1
+    if indice_contato_correto >= 0 and indice_contato_correto < len(minha_agenda):
+        agenda[indice_contato_correto]['telefone'] = telefone
+        print(f'Contato {indice_contato} atualizado com sucesso!')
+    else:
+        print('[ERRO]! O Indice é inválido!')
+        print()
+    return
+
+
+def editar_somente_email(agenda, indice_contato, email):
+    """ Função designada apenas para edição de e-mail do usuário """
+
+    indice_contato_correto = indice_contato - 1
+    if indice_contato_correto >= 0 and indice_contato_correto < len(minha_agenda):
+        agenda[indice_contato_correto]['email'] = email
+        print(f'Contato {indice_contato} atualizado com sucesso!')
+    else:
+        print('[ERRO]! O Indice é inválido!')
+        print()
+    return
     
+
+def editar_contato(agenda, indice_contato, nome, telefone, email):
+    """ Função designada para edição de todos os dados do usuário """
+
+    indice_contato_correto = indice_contato - 1
+    if indice_contato_correto >= 0 and indice_contato_correto < len(minha_agenda):
+        agenda[indice_contato_correto]['nome'] = nome
+        agenda[indice_contato_correto]['telefone'] = telefone
+        agenda[indice_contato_correto]['email'] = email
+        print(f'Contato {indice_contato} atualizado com sucesso!')
+    else:
+        print('[ERRO]! O Indice é inválido!')
+        print()
     return 
 
 
@@ -98,5 +146,40 @@ while True:
                 print()
                 continue
             else:
-                editar_agenda(minha_agenda)
+                visualizar_contatos(minha_agenda)
+                indice_contato = int(input('Qual contato deseja editar: '))
+                print('-' * 20)
+                print('Qual dado abaixo deseja atualizar: ')
+                print('-' * 20)
+                print('1 - Nome')
+                print('2 - Telefone')
+                print('3 - E-mail')
+                print('4 - Todos os dados')
+                print('-' * 20)
 
+                while True:
+                    qual_dado_atualizar = int(input('Digite aqui -> '))
+                    match qual_dado_atualizar:
+                        case 1:
+                            novo_nome = input('Digite o nome desejado: ').title()
+                            editar_somente_nome(minha_agenda, indice_contato, novo_nome)
+                            break
+                        case 2:
+                            novo_contato = input('Digite o CONTATO desejado: ')
+                            editar_somente_telefone(minha_agenda, indice_contato, novo_contato)
+                            break
+                        case 3:
+                            novo_email = input('Digite o E-MAIL desejado: ')
+                            editar_somente_email(minha_agenda, indice_contato, novo_email)
+                            break
+                        case 4:
+                            novo_nome = input('Digite o nome desejado: ').title()
+                            novo_contato = input('Digite o CONTATO desejado: ')
+                            novo_contato = input('Digite o E-MAIL desejado: ')
+                            editar_contato(minha_agenda, indice_contato, novo_nome, novo_contato, novo_email)
+                            break
+                        case _:
+                            print('Digite uma das opções acima!')
+                            print()
+                            continue
+                
