76b02bbefce1b0be555205206577650b49941ed4
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-06-15 17:54:57 -0400

    finalizando exemplos sobre herança simples

--- Módulo 2/pilares_poo.py
+++ Módulo 2/pilares_poo.py
@@ -28,6 +28,67 @@ animais = [dog, cat]
 for animal in animais:
   print(f"{animal.nome} faz: {animal.emitir_som()}")
 
+class Pessoa:
+    def __init__(self, nome='', idade=0):
+        self.nome = nome
+        self.idade = idade
+
+    def fazer_aniversario(self):
+        self.idade += 1
+    
+    def exibir_dados(self):
+        print(f'Nome: {self.nome}| Idade: {self.idade}')
+
+
+class Aluno(Pessoa):
+    def __init__(self, nome='', idade=0, curso='', turma=''):
+        super().__init__(nome, idade)
+        self.curso = curso
+        self.turma = turma
+
+    def fazer_matricula(self):
+        print(f'O Aluno {self.nome} acabou de se matricular.')
+
+
+class Professor(Pessoa):
+    def __init__(self, nome='', idade=0, especialidade='', nivel=''):
+        super().__init__(nome, idade)
+        self.especialidade = especialidade
+        self.nivel = nivel
+
+    def dar_aula(self):
+        print(f'O Prof° {self.nome} começou a dar aula.')
+        pass
+
+
+class Funcionario(Pessoa):
+    def __init__(self, nome='', idade=0, cargo='', setor='', salario=0):
+      super().__init__(nome, idade)
+      self.cargo = cargo
+      self.setor = setor
+      self.salario = salario
+
+    def bater_ponto(self):
+        print(f'A funcionária {self.nome} acabou de bater o ponto.')
+
+    def exibir_dados(self):
+        print(f'Nome: {self.nome} | Idade: {self.idade} | Salário: {self.salario}')
+
+print()
+a1 = Aluno('João', 17, 'Engenharia', '2° Ano')
+a1.fazer_aniversario()
+a1.fazer_matricula()
+
+p1 = Professor('Marcos', 39, 'Engenharia de processos', 'Mestrado')
+p1.fazer_aniversario()
+p1.dar_aula()
+
+f1 = Funcionario('Maria', 28, 'Limpeza', 'B2', 1500)
+f1.fazer_aniversario()
+f1.bater_ponto()
+f1.exibir_dados()
+print()
+
 print("\nExemplo de encapsulamento:")
 class ContaBancaria:
   def __init__(self, saldo) -> None:
@@ -54,3 +115,36 @@ conta.sacar(valor=2000)
 print(f"Saldo da conta bancária: {conta.consultar_saldo()}")
 
 conta_do_zezinho = ContaBancaria(saldo=50)
+
+class Veiculo:
+  def __init__(self, marca, modelo):
+    self.marca = marca
+    self.modelo = modelo
+
+  def ligar_veiculo(self):
+    print(f'O veiculo {self.marca} - {self.modelo} ligou!')
+
+
+class Moto(Veiculo):
+  def __init__(self, marca, modelo):
+    super().__init__(marca, modelo)
+
+  def ligar_veiculo(self):
+    print(f'A moto {self.marca} - {self.modelo} ligou usando os pedais.')
+  
+class Carro(Veiculo):
+  def __init__(self, marca, modelo):
+    super().__init__(marca, modelo)
+
+  def ligar_veiculo(self):
+    print(f'O carro {self.marca} - {self.modelo} ligou usando a chave.')
+
+
+m1 = Moto('Yamaha', 'Factor')
+c1 = Carro('Chevrolet', 'Celta')
+
+m1.ligar_veiculo()
+c1.ligar_veiculo()
+
+
+  
