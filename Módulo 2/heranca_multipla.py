91d269ed39c8ca191b6e1ebff87efd6bc38f11fc
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-06-16 23:32:29 -0300

    cont. herança multipla

--- Módulo 2/heranca_multipla.py
+++ Módulo 2/heranca_multipla.py
@@ -5,9 +5,23 @@ class Animal:
     def emitir_som():
         pass
 
+
 class Mamifero(Animal):
     def amamentar(self):
         return f'{self.nome} está amamentando'
-    
+
+
 class Ave(Animal):
-    pass
+    def voar(self):
+        return f'{self.nome} está voando'
+
+
+class Morcego(Mamifero, Ave):
+    def emitir_som(self):
+        return 'Morcegos emitem sons ultrassônicos'
+    
+
+morcego = Morcego(nome='Batman')
+
+# Acessando métodos da classe base 'Animal
+print('Nome do morcego: ', morcego.nome)
