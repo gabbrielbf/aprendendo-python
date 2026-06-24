dcf00e73dee75e7f74938c2e9a58a24acb343dee
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-06-24 11:37:36 -0400

    sistema curto de herança para mini cafeteria

--- Módulo 2/exercicios/cafeteira.py
+++ Módulo 2/exercicios/cafeteira.py
@@ -1,11 +1,15 @@
 from abc import ABC, abstractmethod
 
 class BebidaQuente(ABC):
-    def __init__(self, nome_bebida=''):
+    def preparar(self, nome_bebida=''):
         self.nome_bebida = nome_bebida
-        print('--- Iniciando Preparo ---')
+        print('\n--- Iniciando Preparo ---')
+        self.ferver_agua()
+        self.misturar()
+        self.servir()
+        print('--- Bebida Pronta ---\n')
         return
-    
+
     def ferver_agua(self):
         print(f'1. Fervendo água para beber ({self.nome_bebida})!')
         return
@@ -38,6 +42,10 @@ class Cha(BebidaQuente):
         return super().servir()
 
 class LeiteQuente(BebidaQuente):
+    def misturar(self):
+        print(f'2. Misturando com colher de pau.')
+        return super().misturar()
+
     def servir(self):
         print(f'3. Servindo em copo térmico.')
         return super().servir()
--- Módulo 2/exercicios/cafeteria_digital.py
+++ Módulo 2/exercicios/cafeteria_digital.py
@@ -1,6 +1,10 @@
 from cafeteira import *
-    
 
+lista_bebidas = ['Cafe', 'Cha', 'Leite']
+
+def main():
+    bebida = LeiteQuente()
+    bebida.preparar(lista_bebidas[2])
 
 if __name__ == '__main__':
     main()
