196ffa2c6b2f2684fd3e0c847496012367187318
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-06-30 13:12:04 -0400

    def function main and some objects

---
+++ exercicios/youtube e demais cursos/conta_bancaria.py
@@ -0,0 +1,10 @@
+from classes import ContaBancaria
+
+def main():
+
+    p1 = ContaBancaria(102, 'Jorge', 2_000)
+    p2 = ContaBancaria(193, 'Claudia', 5_000)
+    return
+
+if __name__ == '__main__':
+    main()
