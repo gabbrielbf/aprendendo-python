9d0ea283861020439f5fbe3a893e1e016eb3c387
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-06-30 12:57:27 -0400

    criando um mini sistema de conta bancária para treinar encapsulamento

--- exercicios/youtube e demais cursos/classes.py
+++ exercicios/youtube e demais cursos/classes.py
@@ -328,4 +328,25 @@ class Mago(Personagem):
         return
 
 
+# Sistema de conta bancária para treinar encapsulamento
+class ContaBancaria:
 
+    def __init__(self, id, nome, saldo=0):
+        self.id = id
+        self.nome = nome
+        self.saldo = saldo
+        print(f'Conta N° {self.id} criada com sucesso\nSaldo atual é de: R${self.saldo:,.2f}')
+    
+    def __str__(self):
+        return f'A conta {self.id} de {self.nome} tem {self.saldo:,.2f} de saldo.'
+    
+    def depositar(self, valor):
+        self.saldo += valor
+        print(f'Depósito de R${valor:,.2f} autorizado na conta {self.id}')
+        return
+
+    def sacar(self, valor):
+        self.saldo -= valor
+        print(f'Saque de R${valor:,.2f} autorizado na conta {self.id}')
+        return
+        
