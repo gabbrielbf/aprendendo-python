d3adbea457cbbb7d1e739c4433664acf68ea535e
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-07-20 19:58:20 -0400

    ajustando lógica da venda

--- exercicios/youtube e demais cursos/inventario.py
+++ exercicios/youtube e demais cursos/inventario.py
@@ -2,6 +2,7 @@ import os, time
 
 # Sistema de inventário de loja
 estoque = []
+valor_compra = 0
 
 def limpar_interface():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -25,6 +26,24 @@ def inserir_produto(nome:str, preco:float, qtd_inicial:int):
     estoque.append({'nome':nome, 'preco':preco, 'qtd_inicial':qtd_inicial})
     return
 
+def exibir_estoque():
+    for indice, produto in enumerate(estoque, start=1):
+        print(f'produto {indice} - {produto['nome']} | preço {produto['preco']} | estoque {produto['qtd_inicial']}')
+    return
+
+def vender_produto(venda, quantidade):
+    for _ in estoque:
+        if venda == estoque['nome']:
+            if quantidade > estoque['qtd_inicial']:
+                valor_compra += estoque['preco']
+                estoque['qtd_inicial'] -= quantidade
+                print(f'você comprou {estoque['nome']} e o valor deu: {valor_compra}')
+            else:
+                raise Exception('estoque insuficiente.')
+        else:
+            raise Exception('produto não encontrado.')
+    return
+
 def main():
     while True:
         limpar_interface()
@@ -37,14 +56,20 @@ def main():
                 except ValueError:
                     raise Exception('valor digitado inválido.')
                 inserir_produto(nome_produto, preco_produto, qtd_produto)
-                print('produto adicioado com sucesso ✔️')
+                print('produto adicionado com sucesso ✔️')
             case 2:
                 if not estoque:
                     raise Exception('estoque vazio!')
-                for indice, produto in enumerate(estoque, start=1):
-                    print(f'produto {indice} - {produto['nome']} | preço {produto['preco']} | estoque {produto['qtd_inicial']}')
+                exibir_estoque()
             case 3:
-                pass
+                exibir_estoque()
+                try:
+                    venda = str(input('o que deseja comprar -> ')).title()
+                    quantidade = int(input('quantas un. deseja comprar -> '))
+                except ValueError:
+                    raise Exception('valor digitado inválido.')
+                
+                vender_produto(venda, quantidade)
             case 4:
                 pass
             case 0:
