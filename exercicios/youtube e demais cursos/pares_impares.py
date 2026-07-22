ab5dd1df04f454e2174440ee375b804fd71cec01
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-07-22 17:19:41 -0400

    refatorando lista de numeros

--- exercicios/youtube e demais cursos/pares_impares.py
+++ exercicios/youtube e demais cursos/pares_impares.py
@@ -8,10 +8,10 @@ def separar_pares_e_impares(lista_numeros):
         else:
             lista_impares.append(numero)
 
-    print(f'lista de pares: {lista_pares}')
-    print(f'lista de impares: {lista_impares}')
-
-    return
+    return lista_pares, lista_impares
 
 lista_numeros = [1, 3, 4, 5, 6, 2, 7, 9, 8, 12, 15, 10, 25, 33, 19]
-separar_pares_e_impares(lista_numeros)
+lista_pares, lista_impares = separar_pares_e_impares(lista_numeros)
+
+print(f'lista de pares: {lista_pares}')
+print(f'lista de impares: {lista_impares}')
