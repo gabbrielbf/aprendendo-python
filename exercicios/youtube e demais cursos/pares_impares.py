f09b507a3736a95db8301178fa67f08a44ab64c0
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-07-22 17:18:17 -0400

    separar pares de impares

---
+++ exercicios/youtube e demais cursos/pares_impares.py
@@ -0,0 +1,17 @@
+def separar_pares_e_impares(lista_numeros):
+    lista_pares = []
+    lista_impares = []
+
+    for numero in lista_numeros:
+        if numero % 2 == 0:
+            lista_pares.append(numero)
+        else:
+            lista_impares.append(numero)
+
+    print(f'lista de pares: {lista_pares}')
+    print(f'lista de impares: {lista_impares}')
+
+    return
+
+lista_numeros = [1, 3, 4, 5, 6, 2, 7, 9, 8, 12, 15, 10, 25, 33, 19]
+separar_pares_e_impares(lista_numeros)
