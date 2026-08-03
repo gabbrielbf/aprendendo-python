04ee3837b8eca83e2c20d06e1a348cf62488d77b
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-03 13:21:39 -0400

    adicionando os dados para primeira exibição e criando método "simetric_search"

--- Conteudos Extra -  estruturas de dados/tree.py
+++ Conteudos Extra -  estruturas de dados/tree.py
@@ -11,21 +11,51 @@ class Node:
     def __str__(self): # <- Serve para retornar uma string do dado fornecido 
         return str(self.data)
     
-class BinaryTree: # <- Resoonável pela parte inteligente da árvore, manipulando os métodos e tratando os dados passados na classe Node()
-    def __init__(self, data):
-        node = Node(data) # Objeto criado para inserir um dado a ser trata-do como a raiz da árvore
-        self.root = node
+class BinaryTree: # <- Responsável pela parte inteligente da árvore, manipulando os métodos e tratando os dados passados na classe Node()
+    def __init__(self, data=None):
+        if data: # <- Modificação implementada para poder usar o BinaryTree sem precisar passar algum dado como parâmetro
+            node = Node(data) # <- Objeto criado para inserir um dado a ser trata-do como a raiz da árvore
+            self.root = node
+        else:
+            self.root = None
 
         return
 
-def main():
-    tree = BinaryTree(4) # <- Definindo a raiz da árvore
-    tree.root.left = Node(11) # <- Valor da esquerda
-    tree.root.right = Node(9) # <- Valor da direita
+    def simetric_search(self):
+        pass
 
-    print(tree.root)
-    print(tree.root.left)
-    print(tree.root.right)
+def main():
+    # tree = BinaryTree(4) # <- Definindo a raiz da árvore
+    # tree.root.left = Node(11) # <- Valor da esquerda
+    # tree.root.right = Node(9) # <- Valor da direita
+
+    # print(tree.root)
+    # print(tree.root.left)
+    # print(tree.root.right)
+
+    tree = BinaryTree()
+    n1 = Node('a')
+    n2 = Node('+')
+    n3 = Node('*')
+    n4 = Node('b')
+    n5 = Node('-')
+    n6 = Node('/')
+    n7 = Node('c')
+    n8 = Node('d')
+    n9 = Node('e')
+
+    n6.left = n7
+    n6.right = n8
+    n5.left = n6
+    n5.right = n9
+    n3.left = n4
+    n3.right = n5
+    n2.left = n1
+    n2.right = n3
+
+    tree.root = n2
+
+    return
 
 if __name__ == '__main__':
     main()
