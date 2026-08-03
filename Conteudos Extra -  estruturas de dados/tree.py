ebb244e4ab0d3040a12f33cdf9e97c7d6ff1b963
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-03 12:10:22 -0400

    teorias iniciais

--- Conteudos Extra -  estruturas de dados/tree.py
+++ Conteudos Extra -  estruturas de dados/tree.py
@@ -1,3 +1,12 @@
 # Começando os estudos do que se tratam árvores no geral
 
-# O que são nós?
+# O que são nós? Ligação de informações não lineares através de uma raiz específica
+# O que são folhas? Nós isolados no fim das árvores sem ligações abaixo deles
+# Ex.
+#        A <- Raiz
+#       / \  
+#      B   C <- Estrutura da árvore
+#           \
+#            D <-  Folha
+#
+# Floresta: São vários conjuntos de nós isolados não ligados um ao outro
