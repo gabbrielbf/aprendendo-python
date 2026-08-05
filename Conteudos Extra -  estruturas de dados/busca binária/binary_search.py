e57719a5b5bc803872402a2662871e79f6409393
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-05 12:57:40 -0400

    programa de exemplificação de binary_search implementado

--- Conteudos Extra -  estruturas de dados/busca binária/binary_search.py
+++ Conteudos Extra -  estruturas de dados/busca binária/binary_search.py
@@ -1,1 +1,19 @@
 # Binary Search
+
+def binary_search(arr, item, begin=0, end=None):
+
+    if end is None:
+        end = len(arr) - 1
+
+    if begin <= end:
+        mid =  (begin + end) // 2
+        if arr[mid] == item:
+            return mid
+        
+        if item < arr[mid]:
+            return binary_search(arr, item, begin, mid - 1)
+        
+        else:
+            return binary_search(arr, item, mid + 1, end)
+
+    return None
