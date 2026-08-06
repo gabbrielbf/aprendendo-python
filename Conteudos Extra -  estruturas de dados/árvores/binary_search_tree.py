be83965a99eddf7900832542d1da9c4b6f2f24bd
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-06 19:30:22 -0400

    testando resultados da implementação, mas aguardando refatoração da visualização da árvore

--- Conteudos Extra -  estruturas de dados/árvores/binary_search_tree.py
+++ Conteudos Extra -  estruturas de dados/árvores/binary_search_tree.py
@@ -1,3 +1,13 @@
 # Implementação de uma árvore binária de busca
 
-from binary_tree import BinaryTree, Node
+import random
+from binary_tree import BinarySearchTree
+
+values = random.sample(range(1, 1001), 42) # <- função para retornar uma LISTA de 42 números inteiros únicos e sem repetição 
+                                           # em um intervalo de 1 até 1000
+binary_search_tree = BinarySearchTree()
+
+for value in values:
+    binary_search_tree.insert(value)
+
+binary_search_tree.inorder_search()
